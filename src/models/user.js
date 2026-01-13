c4d7422ec13400999ff1ad9e3810e2f451810728
Bless <tetaangelbless014@gmail.com>
2026-01-13 22:13:53 +0200

    Initial commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,12 @@
 const mongoose = require('mongoose');
 
-const userSchema
+const userSchema=new mongoose.Schema({
+    email :{
+        type:String,
+        required: true,
+        unique:true
+    },
+    password:{
+        type:String;
+    }
+})
