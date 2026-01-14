7083c0f4a796460bc572cb8ee19fb7824fce651e
Bless <tetaangelbless014@gmail.com>
2026-01-14 08:13:56 +0200

    Initial commit

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -9,7 +9,7 @@ const User = require('../models/user');
         const {email,password}= req.body;
         const existingUser=await User.findOne({email});
         if (existingUser){
-            return res.status(400).json
+            return res.status(400).json({message:'User already exists.'});
         }
     }
  })
