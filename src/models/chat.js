b2efb879bc50c7c480319b912b024ecaf5de2eaa
Bless <tetaangelbless014@gmail.com>
2026-01-16 09:36:26 +0200

    Initial commit

--- src/models/chat.js
+++ src/models/chat.js
@@ -2,4 +2,19 @@ const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema({
       participants: [
- 
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  mode: {
+    type: String,
+    enum: ['project', 'study', 'casual','meeting'],
+    default: 'casual'
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Chat', chatSchema);
+
