e4ac287302f0447b047310907ee3a830906941bb
Bless <tetaangelbless014@gmail.com>
2026-01-16 15:29:30 +0200

    Initial commit

--- src/models/Message.js
+++ src/models/Message.js
@@ -1,6 +1,7 @@
 const mongoose= require ('mongoose');
 const messageSchema= new mongoose.Schema({
     chat:{
-        type:mongoose.Schema.Types.ObjectId
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Chat'
     }
 })
