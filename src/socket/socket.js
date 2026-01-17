954638df1001d3e5a015e55d852101a551f9848a
Bless <tetaangelbless014@gmail.com>
2026-01-17 21:12:44 +0200

    Initial commit

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -4,7 +4,8 @@ const socketHandler =(io)=>{
         console.log('User connected:', socket.id);
         socket.on('joinChat',(chatId)=>{
             socket.join(chatId);
-            console.log(`User joined ${chatId}`)
+            console.log(`User joined ${chatId}`);
+            
 
         })
 
