243afbbe4345a9ae5a2ed864d59481e8053271d7
Bless <tetaangelbless014@gmail.com>
2026-01-17 21:01:43 +0200

    Initial commit

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -2,6 +2,7 @@ const Message= require('../models/Message');
 const socketHandler =(io)=>{
     io.on('connection',(socket)=>{
         console.log('User connected:', socket.id);
+        socket.on
 
     })
 
