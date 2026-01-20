4b78a6fcd1a878cc37fe7638072b2d561b5c665c
Bless <tetaangelbless014@gmail.com>
2026-01-20 17:35:20 +0200

    Initial commit

--- src/services/contextProcessor.js
+++ src/services/contextProcessor.js
@@ -1,11 +1,12 @@
 const batchMessages = require("../context/batcher");
+const summarizeMessages = require("../context/summarizer");
 
 const contextProcessor= async (chatId)=>{
     if(!chatId){
         throw new Error('ChatId is required');
     }
     const  messages =await batchMessages(chatId);
-    const sumary =null;
+    const sumary =;
     const tasks=[];
     return {
         messages,
