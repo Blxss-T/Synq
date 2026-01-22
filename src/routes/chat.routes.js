f628dfefc20690f95b0f1bd83f760dcc5d6d535a
Bless <tetaangelbless014@gmail.com>
2026-01-22 10:48:17 +0200

    Initial commit

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -7,6 +7,16 @@ router.get('/:chatId/context',async (req,res)=>{
     try{
         const {chatId} = req.params;
         const chat= await Chat.findById(chatId).select('lastSummary tasks');
+        if (!chat){
+            return res.status(404).json({message:'Chat n'})
+        }
+         res.json({
+      summary: chat.lastSummary,
+      tasks: chat.tasks,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
     }
 })
 
