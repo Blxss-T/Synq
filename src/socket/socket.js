29924bd62ccd0990fc05fa5576a52be3c6453488
Bless <tetaangelbless014@gmail.com>
2026-01-22 10:00:22 +0200

    Initial commit

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,40 +1,40 @@
-const { Socket } = require('socket.io');
-const chat = require('../models/chat');
-const Message= require('../models/Message');
-const contextProcessor= require('../services/contextProcessor');
-const socketHandler =(io)=>{
-    io.on('connection',(socket)=>{
-        console.log('User connected:', socket.id);
-        socket.on('joinChat',(chatId)=>{
-            socket.join(chatId);
-            console.log(`User joined ${chatId}`)
-        });
-        socket.on('sendMessage', async(data)=>{
-            try{
-                  const{ chatId,senderId, content }=data;
-            const message =await Message.create({
-                chat:chatId,
-                sender: senderId,
-                content
+const Message = require('../models/Message');
+const contextProcessor = require('../services/contextProcessor');
 
-            });
-            const context =await contextProcessor(chatId);
-            
-            io.to(chatId).emit('receivedMessage', {
-                message,
-                context,
-            });
-                
-            }
-            catch(error){
-                console.log('sendMessage error:',error.message);
-            }
-            });
-                socket.on('disconnect',()=>{
-                    console.log('User disconnected:',socket.id); 
-                });
+const socketHandler = (io) => {
+  io.on('connection', (socket) => {
+    console.log('🟢 User connected:', socket.id);
 
-          
+    socket.on('joinChat', (chatId) => {
+      socket.join(chatId);
+      console.log(`User joined ${chatId}`);
     });
+
+    socket.on('sendMessage', async (data) => {
+      try {
+        const { chatId, senderId, content } = data;
+
+        const message = await Message.create({
+          chat: chatId,
+          sender: senderId,
+          content,
+        });
+
+        const context = await contextProcessor(chatId);
+
+        io.to(chatId).emit('receivedMessage', {
+          message,
+          context,
+        });
+      } catch (error) {
+        console.error('sendMessage error:', error.message);
+      }
+    });
+
+    socket.on('disconnect', () => {
+      console.log('User disconnected:', socket.id);
+    });
+  });
 };
-module.exports= socketHandler;
+
+module.exports = socketHandler;
