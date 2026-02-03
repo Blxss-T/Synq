66aa98becbaa82d914d94a87fb96a34898b1b80f
Bless <tetaangelbless014@gmail.com>
2026-02-03 16:21:25 +0200

    Initial commit

--- src/models/Message.js
+++ src/models/Message.js
@@ -17,4 +17,4 @@ const messageSchema= new mongoose.Schema({
 },{timestamps:true
 
 });
-module.exports= mongoose.model('Message',schemaMessage)
+module.exports= mongoose.model('Message',messageSchema)
